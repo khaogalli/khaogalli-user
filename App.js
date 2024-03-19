f6213f52374dc007d993f03decdcce81e9564a4e
Heet <heetjhaveri61001@gmail.com>
2024-03-19 19:37:28 +0530

    add restaurant

--- App.js
+++ App.js
@@ -1,16 +1,14 @@
 import React, { useState } from "react";
 import Api from "./src/ApiManager";
 import Home from "./src/screens/Home";
+import Restaurants from "./src/screens/Restaurant";
 import Signin from "./src/screens/Signin";
 import Signup from "./src/screens/Signup";
 
 const App = () => {
   const [isSignedIn, setIsSignedIn] = useState(false);
   Api.onAuthChange = setIsSignedIn;
-  return (
-    isSignedIn ?
-      (<Home />) : (<Signin />)
-  )
-}
+  return isSignedIn ? <Home /> : <Restaurant />;
+};
 
 export default App;
