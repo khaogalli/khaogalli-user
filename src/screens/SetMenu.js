2a83674fc73e05911ffb22ef3f5f4d922661ecb4
Heet <heetjhaveri61001@gmail.com>
2024-06-16 11:38:41 +0530

    have to add remove button and styling of Done button also Scroll view and keyboard avoiding view to be added

--- src/screens/SetMenu.js
+++ src/screens/SetMenu.js
@@ -9,81 +9,218 @@ import {
   SafeAreaView,
   TextInput,
   Pressable,
+  KeyboardAvoidingView,
+  Switch,
+  Platform,
 } from "react-native";
 import { DataTable } from "react-native-paper";
 
 export default function App() {
-  const [menu, setMenu] = useState([]);
-  const Menu = [
-    { id: "1", name: "Item 1", price: 10, status: 1 },
-    { id: "2", name: "Item 2", price: 15, status: 0 },
-    { id: "3", name: "Item 3", price: 20, status: 1 },
-  ];
+  //id is the decoded form of the item id stores in the database. data base will have id as resname+itemid and id will be the itemid
+  //thus making id unique as well as easy to predict.
+  const [Menu, setMenu] = useState([
+    { id: "1", name: "Item 1", price: 10, status: true },
+    { id: "2", name: "Item 2", price: 15, status: false }, //from api
+    { id: "3", name: "Item 3", price: 20, status: true },
+  ]);
+
+  const [name, setName] = useState("");
+  const [price, setPrice] = useState(0);
+
+  const toggleSwitch = (id) => {
+    setMenu((prevMenu) =>
+      prevMenu.map((item) =>
+        item.id === id ? { ...item, status: !item.status } : item
+      )
+    );
+  };
+
+  addItems = () => {
+    let id = String(parseInt(Menu.at(-1).id) + 1);
+    let newItem = {
+      id: id,
+      name: name,
+      price: price,
+      status: true,
+    };
+    let tempMenu = Menu;
+    tempMenu.push(newItem);
+    console.log(newItem);
+    setMenu(tempMenu);
+    console.log(Menu);
+  };
 
   const renderItem = ({ item }) => (
     <>
-      {item.status ? (
-        <Pressable
-          onPress={() => {
-            console.log(item.OderID);
-          }}
+      <Pressable
+        onPress={() => {
+          console.log(item.OderID);
+        }}
+      >
+        <View
+          style={[
+            {
+              padding: 15,
+              marginBottom: 7,
+              margin: 2,
+              borderRadius: 20,
+              flexDirection: "row",
+              alignItems: "center",
+              backgroundColor: "white",
+              width: "100%",
+            },
+            {
+              shadowColor: "#000",
+              shadowOffset: {
+                width: 0,
+                height: 2,
+              },
+              shadowOpacity: 0.25,
+              shadowRadius: 3.84,
+              elevation: 5,
+            },
+          ]}
         >
-          <View
-            style={[
-              {
-                padding: 15,
-                marginBottom: 7,
-                margin: 2,
-                borderRadius: 20,
-                flexDirection: "row",
-                alignItems: "center",
-                backgroundColor: "white",
-              },
-              {
-                shadowColor: "#000",
-                shadowOffset: {
-                  width: 0,
-                  height: 2,
-                },
-                shadowOpacity: 0.25,
-                shadowRadius: 3.84,
-
-                elevation: 5,
-              },
-            ]}
-          >
-            <View style={{ padding: 10 }}>
-              <Text>Order ID: {item.OderID}</Text>
-              <Text> Customer ID: {item.UserID}</Text>
-            </View>
-            <View style={{ padding: 10, position: "absolute", right: 0 }}>
-              <Text>{item.Date}</Text>
-              <Text>{item.Time}</Text>
-            </View>
+          <View style={{ padding: 10 }}>
+            <Text>Item {item.name}</Text>
+            <Text>Price {item.price}</Text>
           </View>
-        </Pressable>
-      ) : null}
+          <View style={{ padding: 10, position: "absolute", right: 0 }}>
+            <Switch
+              trackColor={{ false: "#767577", true: "#81b0ff" }}
+              thumbColor={item.status ? "#f5dd4b" : "#f4f3f4"}
+              ios_backgroundColor="#3e3e3e"
+              onValueChange={() => {
+                toggleSwitch(item.id);
+              }}
+              value={item.status}
+            />
+          </View>
+        </View>
+      </Pressable>
     </>
   );
 
   return (
-    <SafeAreaView style={{ flex: 1 }}>
-      <View>
-        <Text>Set Menu</Text>
-      </View>
-      <View style={styles.bottomView}>
-        <FlatList
-          style={{ width: "100%", marginTop: 5 }}
-          data={Menu}
-          renderItem={renderItem}
-          keyExtractor={(item, index) => index.toString()}
-        />
-      </View>
-      <View>
-        <TextInput style={styles.input} placeholder="Item Name" />
-        <TextInput style={styles.input} placeholder="Price" />
-      </View>
-    </SafeAreaView>
+    <KeyboardAvoidingView
+      behavior={Platform.OS === "ios" ? "padding" : "height"}
+      style={{
+        flex: 1,
+      }}
+    >
+      <SafeAreaView style={{ flex: 1 }}>
+        <StatusBar backgroundColor="#ad8840" />
+        <View style={styles.container}>
+          <View
+            style={[
+              styles.topView,
+              { flexDirection: "row", justifyContent: "space-around" },
+            ]}
+          >
+            <Text
+              style={{
+                fontSize: 28,
+                textAlign: "center",
+              }}
+            >
+              Edit Menu
+            </Text>
+            <View>
+              <TouchableOpacity
+                styles={{
+                  height: 40,
+                  backgroundColor: "#ffbf00",
+                  borderWidth: 1,
+                  padding: 10,
+                  borderRadius: 10,
+                  marginTop: 25,
+                  justifyContent: "center",
+                }}
+              >
+                <Text>Done</Text>
+              </TouchableOpacity>
+            </View>
+          </View>
+          <View>
+            <View>
+              <FlatList
+                data={Menu}
+                renderItem={renderItem}
+                keyExtractor={(item) => item.id}
+                style={{ padding: 2 }}
+              />
+            </View>
+            <View
+              style={{
+                flexDirection: "row",
+                justifyContent: "space-between",
+                paddingLeft: 10,
+                marginTop: 10,
+              }}
+            >
+              <View
+                style={{
+                  flex: 1,
+                  alignItems: "center",
+                }}
+              >
+                <Text style={styles.lable}>Item</Text>
+                <TextInput
+                  style={[styles.input, { height: 40, width: 150 }]}
+                  onChangeText={setName}
+                  value={name}
+                />
+              </View>
+              <View
+                style={{
+                  flex: 1,
+                  alignItems: "center",
+                }}
+              >
+                <Text style={styles.lable}>Price</Text>
+                <TextInput
+                  style={[styles.input, { height: 40, width: 100 }]}
+                  onChangeText={setPrice}
+                  value={price}
+                />
+              </View>
+              <View
+                style={{
+                  flex: 1,
+                  alignItems: "center",
+                }}
+              >
+                <TouchableOpacity
+                  style={{
+                    height: 40,
+                    backgroundColor: "#ffbf00",
+                    borderWidth: 1,
+                    padding: 10,
+                    borderRadius: 10,
+                    marginTop: 25,
+                    justifyContent: "center",
+                  }}
+                  onPress={() => {
+                    addItems();
+                  }}
+                >
+                  <Text
+                    style={{
+                      textAlign: "center",
+                      fontWeight: "bold",
+                      fontSize: 15,
+                      width: 100,
+                    }}
+                  >
+                    Add
+                  </Text>
+                </TouchableOpacity>
+              </View>
+            </View>
+          </View>
+        </View>
+      </SafeAreaView>
+    </KeyboardAvoidingView>
   );
 }
 
@@ -133,16 +270,12 @@ const styles = StyleSheet.create({
     borderWidth: 1,
     padding: 10,
     borderRadius: 10,
-    backgroundColor: "#431213",
-    color: "white",
+    color: "black",
   },
   lable: {
     fontSize: 18,
-    color: "white",
+    color: "black",
     alignItems: "center",
-    justifyContent: "center",
-    textAlign: "center",
-    paddingRight: 10,
     fontWeight: "bold",
     marginBottom: 2,
   },
